545734f731fb72d3af9930896de4ecf772548a75
elliot sherman <sheveryosef@gmail.com>
2022-08-10 14:56:14 +0300

    add files to github

---
+++ js/meme.service.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+let gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
