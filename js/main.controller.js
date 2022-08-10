90f0eb79231af7e2d24daa18a5e20a98e738028a
elliot sherman <sheveryosef@gmail.com>
2022-08-10 16:58:52 +0300

    add draw function

--- js/main.controller.js
+++ js/main.controller.js
@@ -1,12 +1,17 @@
 'use strict'
 
 let elGalleryGrid
+let elCanvas
+let gCtx 
 
 init()
 
 function init() {
+    gCtx = document.getElementById('canvas').getContext('2d')
     elGalleryGrid = document.querySelector('.main-gallery')
+    elCanvas = document.querySelector('#canvas')
     renderGallery()
+    draw()
 }
 
 
@@ -14,3 +19,9 @@ function onImageClick(el) {
     console.log(el);
     console.log('moving pic to canvas');
 }
+
+function draw() {
+    
+    gCtx.font = '48px serif';
+    gCtx.fillText('Hello world', 10, 50);
+  }
