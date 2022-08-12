092d55fad661d4e11d95246be78cdb94302e5daf
elliot sherman <sheveryosef@gmail.com>
2022-08-12 18:22:49 +0300

    add icons to all the the control buttons

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,23 +1,24 @@
 'use strict'
 
-function onInputChange(type, val){
+function onInputChange(type, val) {
     inputChange(type, val)
 }
 
-function onHandleTxtSize(val){
+function onHandleTxtSize(val) {
     handleTxtSize(val)
 }
 
-function onHandleAlignChange(val){
+function onHandleAlignChange(val) {
     handleAlignChange(val)
 }
-function handleFontChange(val){
+function handleFontChange(val) {
     fontChange(val)
 }
 function clearCanvas() {
     document.querySelector('.main-gallery').classList.remove('hide')
     document.querySelector('.main-editor').classList.add('hide')
     gCtx.clearRect(0, 0, elCanvas.width, elCanvas.height);
+    handleActive(gElNavlinks[0])
     // You may clear part of the canvas
     // gCtx.clearRect(0, 0, gElCanvas.width / 2, gElCanvas.height / 2);
 }
